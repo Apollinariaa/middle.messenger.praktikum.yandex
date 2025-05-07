b8257c199f31939d57552ec50222dfe774478b40
Roslyakova Polina <czadma.rosliakova@globus.cz>
2025-05-07 22:03:35 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
+  plugins: [handlebars()],
   server: { port: 3000, },
+  preview: { port: 3000 },
   build: {
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, 'index.html'),
-      },
-    },
+    outDir: "dist"
   },
 });
