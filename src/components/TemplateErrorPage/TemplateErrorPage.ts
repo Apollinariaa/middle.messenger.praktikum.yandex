5a75311aa8830fef447362707d91565a70153758
Roslyakova Polina <czadma.rosliakova@globus.cz>
2025-05-07 21:16:36 +0300

    fix

--- src/components/TemplateErrorPage/TemplateErrorPage.ts
+++
@@ -1,5 +0,0 @@
-export const template = `
-    <h1 class="error-page__title">{{{code}}}</h1>
-    <p class="error-page__comment">{{{comment}}}</p>
-    {{{children}}}
-`;
--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        index: resolve(__dirname, 'index.ts'),
-        profile: resolve(__dirname, 'src/pages/profile/profile/index.ts'),
-        not_found: resolve(__dirname, 'src/pages/404/index.ts'),
-        server_error: resolve(__dirname, 'src/pages/500/index.ts'),
-        auth: resolve(__dirname, 'src/pages/auth/index.ts'),
-        registration: resolve(__dirname, 'src/pages/registration/index.ts'),
-        chat: resolve(__dirname, 'src/pages/chat/index.ts'),
-        change_data: resolve(__dirname, 'src/pages/profile/changeData/index.ts'),
-        change_password: resolve(__dirname, 'src/pages/profile/changePassword/index.ts'),
+        index: resolve(__dirname, 'index.html'),
       },
     },
   },
